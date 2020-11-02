ced9f6d28887caf3d10b6fa4d4f67fed1b49316c
Clea Yates <cyates@lcs.on.ca>
2020-11-02 11:08:02 -0500

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Yates, Clea on 2020-11-02.
+//  Copyright © 2020 Yates, Clea. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
